697539750e4c4a999516b1bab7e52fb9a96fc078
reedHam <reed.hambrook@gmail.com>
2016-10-11 00:14:15 -0600

    added contact me

--- home.php
+++ home.php
@@ -6,7 +6,9 @@
         <link rel="stylesheet" href="styles/style-main.css">
         <!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src="https://use.fontawesome.com/7db3c32b44.js"></script>
     </head>
+
     <body>
 		<div class="splash-container">
 		    <div class="splash">
@@ -16,7 +18,8 @@
 		        </p>
 		    </div>
 		</div>
-
+		
+		<!--about me-->
 		<div class="content-wrapper">
 			<div class="content">
 				<h1 class="content-head center">About Me</h1>
@@ -41,12 +44,63 @@
 				</div>
 				<span class="grad-line"></span>
 			</div>
-
+			
+			<!--projects-->
 			<div class="content">
 				<h1 class="content-head center">projects</h1>
 				<span class="grad-line"></span>
+					<div class="row">
+						<div class="col-sm-12 col-md-4" >
+							<div class="box">
+								<img src="http://placehold.it/350x450" alt="Reed Hambrook" class="img-responsive center-block">
+							</div>
+						</div>
 
+						<div class="col-sm-12 col-md-4" >
+							<div class="box">
+								<img src="http://placehold.it/350x450" alt="Reed Hambrook" class="img-responsive center-block">
+							</div>
+						</div>
+
+						<div class="col-sm-12 col-md-4" >
+							<div class="box">
+								<img src="http://placehold.it/350x450" alt="Reed Hambrook" class="img-responsive center-block">
+							</div>
+						</div>
+					</div>
 				<span class="grad-line"></span>
+			</div>
+
+			<!--contact me-->
+			<div class="content">
+				<h1 class="content-head center">contact me</h1>
+				<span class="grad-line"></span>
+
+				<div class="row">
+
+					<div class="col-sm-12 col-md-4 text-center">
+						<span class="glyphicon glyphicon-envelope icon-size center"></span>
+						<h4 class="center">Reed.Hambrook@gmail.com</h4>
+					</div>
+					
+					<div class="col-sm-12 col-md-4 text-center">
+						<span class="fa fa-github icon-size" aria-hidden="true"></span>
+						<a href="https://github.com/reedHam" target="_blank"><h4>github.com/reedHam <i class="fa fa-external-link" aria-hidden="true"></i></h4></a>
+					</div>
+
+					<div class="col-sm-12 col-md-4 text-center">
+						<i class="fa fa-linkedin icon-size" aria-hidden="true"></i>
+						<a href="https://www.linkedin.com/in/reed-hambrook" target="_blank"><h4>www.linkedin.com/in/Reed-Hambrook <i class="fa fa-external-link" aria-hidden="true"></i></h4></a>
+					</div>
+
+				</div>
+				
+			</div>
+
+			<div class="footer">
+				<div class="content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid fugit explicabo, inventore eveniet necessitatibus quisquam voluptas. Dolores voluptate labore officia temporibus error reprehenderit, quae asperiores, explicabo commodi tempora, quibusdam voluptatum.
+				</div>
 			</div>
 
 		</div>
