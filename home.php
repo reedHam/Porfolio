7de078ab17bec23d256a7e0869545698aad38212
reedHam <reed.hambrook@gmail.com>
2016-10-10 21:12:57 -0600

    Website added to repository
    started using git

---
+++ home.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Reed Hambrook</title>
+        <link rel="stylesheet" href="styles/style-main.css">
+    </head>
+    <body>
+		<div class="splash-container">
+		    <div class="splash">
+		        <h1 class="splash-head">Reed Hambrook</h1>
+		        <p class="splash-subhead">
+		            Computer Science Student at the Univercity of Lethbridge, Alberta
+		        </p>
+		    </div>
+		</div>
+
+		<div class="content-wrapper">
+			<div class="content">
+				<h1 class="content-head center">About Me</h1>
+				<span class="grad-line"></span>
+				
+				<div class="pure-g">
+
+					<div class="pure-u-1 pure-u-lg-1-5">
+						<div class="box center">
+							<img src="http://placehold.it/350x450" alt="Reed Hambrook" class="pure-img">
+						</div>
+					</div>
+
+					<div class="pure-u-1 pure-u-lg-4-5">
+						<div class="box">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam aut, cumque amet. Possimus recusandae itaque laboriosam nesciunt voluptates veniam aspernatur voluptate! Ipsa optio aliquam incidunt dolorum amet illum consectetur impedit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus deserunt magnam, neque, est suscipit reiciendis a numquam odit. Animi facere ratione iste temporibus odio est nemo, itaque aperiam rem dignissimos.
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<span class="grad-line"></span>
+			</div>
+
+			<div class="content">
+				<h1 class="content-head center">projects</h1>
+				<span class="grad-line"></span>
+				
+				<div class="pure-g">
+					<div class="pure-u-1 pure-u-lg-1-3">
+						<div class="box center">
+							<img src="http://placehold.it/350x450" alt="Reed Hambrook" class="pure-img">
+						</div>
+					</div>
+
+					<div class="pure-u-1 pure-u-lg-1-3">
+						<div class="box center">
+							<img src="http://placehold.it/350x450" alt="Reed Hambrook" class="pure-img">
+						</div>
+					</div>
+
+					<div class="pure-u-1 pure-u-lg-1-3">
+						<div class="box center">
+							<img src="http://placehold.it/350x450" alt="Reed Hambrook" class="pure-img">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
